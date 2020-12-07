8c3978fd12f4b3617656eae418be15aa9f8dbb19
shehan82 <shehansandeepa82@gmail.com>
2020-12-08 00:51:16 +0530

    create grade component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
+import {Button} from '@material-ui/core';
+import Grade from './components/Grade'
 
 function App() {
   return (
     <div className="App">
        <div className="App__header">
-              <h2>upper</h2>
+              <h3>ScHOolY</h3>
        </div>
+
+
         <div className="App__bodyContainer">
           <div className="App__sideBar">
-            <h2>sideBar</h2>
+            <h3>gRadeS</h3><br/>
+            <Grade/>
+            <Grade/>
+            <Grade/>
+            <Grade/>
           </div>
+
+
+
           <div className="App__body">
-            <h2>appbody</h2>
+            <h3>appbody</h3>
           </div>
         </div>
       
---
+++ src/components/Grade.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from '@material-ui/core';
+import '../css/Grade.css';
+
+function Grade() {
+    return (
+        <div className="Grade__container">
+            <Button style={{color:"white", width:"100%"}}>Grade</Button>
+        </div>
+    )
+}
+
+export default Grade
