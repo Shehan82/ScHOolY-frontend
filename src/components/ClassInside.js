95e3df508bd83475637df70f2677617966831d83
shehan82 <shehansandeepa82@gmail.com>
2020-12-09 23:27:28 +0530

    add some

--- src/components/ClassInside.js
+++ src/components/ClassInside.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../css/ClassInside.css'
 
 function ClassInside({match}) {
     return (
         <div>
+            <div className="ClassInside__headerContainer">
+                <div className="ClassInside__headerGrade">
+                    
+                    <h3>Grade {match.params.grade}</h3> 
+                </div>
+                <div className="ClassInside__headerSlash">
+                    <h3>/</h3>
+                </div>
+                <div className="ClassInside__headerClass">
+                    <h3>Class {match.params.class}</h3>
+                </div>
+
+            </div>
+           
+            
+
             {console.log(match.params)}
-            Grade : {match.params.grade}
-            <br/>
-            <br/>
-            class : {match.params.class}
+            
         </div>
     )
 }
