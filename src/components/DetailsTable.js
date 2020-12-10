430040f0db5928a87c2038333a989f1f2865c39f
shehan82 <shehansandeepa82@gmail.com>
2020-12-11 00:04:05 +0530

    change the table fields

--- src/components/DetailsTable.js
+++ src/components/DetailsTable.js
@@ -17,16 +17,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(index, fullName, view) {
+  return { index, fullName, view };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Ice cream sandwich', 237, 9.0),
+  createData('Eclair', 262, 16.0),
+  createData('Cupcake', 305, 3.7),
+  createData('Gingerbread', 356, 16.0),
 ];
 
 
@@ -37,23 +37,19 @@ function DetailsTable() {
         <Table className={classes.table} aria-label="simple table">
           <TableHead style={{backgroundColor:"black"}}>
             <TableRow >
-              <TableCell style={{color:"white"}} >Dessert (100g serving)</TableCell>
-              <TableCell style={{color:"white"}} align="right">Calories</TableCell>
-              <TableCell style={{color:"white"}} align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell style={{color:"white"}} align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell style={{color:"white"}} align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell style={{color:"white"}} >Index Number</TableCell>
+              <TableCell style={{color:"white"}} align="right">Full Name</TableCell>
+              <TableCell style={{color:"white"}} align="right">View Details</TableCell>
             </TableRow>
           </TableHead>
           <TableBody >
             {rows.map((row) => (
-              <TableRow key={row.name}>
+              <TableRow key={row.index}>
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.index}
                 </TableCell>
-                <TableCell  align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell  align="right">{row.fullName}</TableCell>
+                <TableCell align="right">{row.view}</TableCell>
               </TableRow>
             ))}
           </TableBody>
