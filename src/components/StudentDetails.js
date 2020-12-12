13af3b96790e804a6145eecd951ba48f77200ebb
shehan82 <shehansandeepa82@gmail.com>
2020-12-12 20:03:50 +0530

    SemResult.js added

--- src/components/StudentDetails.js
+++ src/components/StudentDetails.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import '../css/StudentDetails.css'
 
-function StudentDetails() {
+function StudentDetails({match}) {
     return (
         <div>
-            I am student 
+            <div className="StudentDetails__headerContainer">
+                <div className="StudentDetails__headerGrade">
+                <Link to={`/grade/${match.params.grade}`} style={{color:"white", textDecoration:"none"}}>
+                    <h3>Grade {match.params.grade}</h3> 
+                </Link >
+                </div>
+                <div className="StudentDetails__headerSlash">
+                    <h3>/</h3>
+                </div>
+                
+                <div className="StudentDetails__headerClass">
+                    
+                <Link to={`/grade/${match.params.grade}/${match.params.class}`} style={{color:"white", textDecoration:"none"}}>
+                    <h3>Class {match.params.class}</h3>
+                </Link >
+                </div>
+
+                <div className="StudentDetails__headerSlash">
+                    <h3>/</h3>
+                </div>
+
+                <div className="StudentDetails__headerStudent">
+                    
+                    <h3>{match.params.student}</h3>
+
+                </div>
+
+            </div>
         </div>
     )
 }
