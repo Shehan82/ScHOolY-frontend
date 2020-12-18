f4afa51064864a39daade756d675657399462b3c
shehan82 <shehansandeepa82@gmail.com>
2020-12-18 17:14:36 +0530

    added input fields for add student page

--- src/components/Home/AddNewStudent.js
+++ src/components/Home/AddNewStudent.js
@@ -1,9 +1,148 @@
-import React from 'react'
+import React from 'react';
+import '../../css/AddNewStudent.css';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+
 
 function AddNewStudent() {
+
+    const sendDetails = (e)=>{
+        e.preventDefault();
+        console.log(e)
+
+    }
+
+  
     return (
+        
         <div>
-            hello i am add new student!
+            <div className="AddNewStudent__header">
+                <h3>Add New Student</h3>
+            </div>
+            <div className="AddNewStudent__formContainer">
+                <form >
+                <h2>Main details</h2> 
+                    <div className="mainDetails">
+                       
+                        <div className="left">
+                            <div className="leftInside">
+                                <label htmlFor="index">Index</label>
+                                <input id="index" name="index"  type="text"/>
+                            </div>
+
+                            <div className="leftInside">
+                            <label htmlFor="fullName">fullName</label>
+                            <input id="fullName" name="fullName" type="text"/>
+                            </div>
+
+                            <div className="leftInside">
+                            <label htmlFor="address">address</label>
+                            <input id="address"  type="text"/>
+                            </div>
+                            
+                           <div className="leftInside">
+                           <label htmlFor="landNum">Land Number</label>
+                            <input id="landNum" type="text"/>
+                           </div>
+                            
+                            
+                        </div>
+
+                        <div className="right">
+                            <div className="rightInside">
+                            <label htmlFor="mobileNum">mobileNum</label>
+                            <input id="mobileNum" type="text"/>
+                            </div>
+
+                            <div className="rightInside">
+                            <label htmlFor="motherName">motherName</label>
+                            <input id="motherName" type="text"/>
+                            </div>
+
+                            <div className="rightInside">
+                            <label htmlFor="fatherName">fatherName</label>
+                            <input id="fatherName" type="text"/>
+                            </div>
+
+                            
+                            
+                        </div>  
+                        
+                    </div>
+                    <br/> <br/>
+
+                <h2>Sem results</h2>
+
+                    <div className="semResult">
+                        
+
+                    <div className="sem">
+                        <h4>Semester 1</h4>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                    </div>
+
+                    <div className="sem">
+                        <h4>Semester 2</h4>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                    </div>
+
+                    <div className="sem">
+                        <h4>Semester 3</h4>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                        <div className="sem_subject">
+                            <input type="text" placeholder="subject"/>  : <input type="text" placeholder="marks"/>
+                        </div>
+                    </div>
+
+                    </div>
+
+                    
+                    
+                        <input onClick={sendDetails} type="submit"/>
+                </form>
+                
+              
+            </div>
         </div>
     )
 }
