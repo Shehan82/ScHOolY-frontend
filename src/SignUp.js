ae1005e7063c6d0e1eb196e7f38d06b4c527f17b
shehan82 <shehansandeepa82@gmail.com>
2021-04-17 22:43:08 +0530

    signup page ui done

--- src/SignUp.js
+++ src/SignUp.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./css/Login.css";
 import axios from "./axios";
 import { auth } from "./components/firebase";
+import Button from "@material-ui/core/Button";
 
 function SignUp() {
   const [email, setemail] = useState("");
@@ -17,26 +18,55 @@ function SignUp() {
     });
   };
   return (
-    <div>
-      <input
-        onChange={(e) => {
-          setemail(e.target.value);
-        }}
-        type="email"
-        placeholder="email"
-      />
-      <input
-        onChange={(e) => {
-          setpassword(e.target.value);
-        }}
-        type="password"
-        name=""
-        id=""
-        placeholder="password"
-      />
-      <Link to={`/`}>
-        <button onClick={sendData}>ksjfsjkhdf</button>
-      </Link>
+    <div className="container">
+      <div className="box_container">
+        <div className="box_header">
+          <h3>ScHOolY - SIGN UP</h3>
+        </div>
+        {error ? (
+          <div className="error_div">{error}</div>
+        ) : (
+          <div className="notError_div"></div>
+        )}
+
+        <div className="box_content">
+          <input
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+            type="email"
+            placeholder="Email"
+            id="email"
+          />
+          <input
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+            type="password"
+            name=""
+            id="password"
+            placeholder="Password"
+          />
+
+          <Link className="btn" to={`/`}>
+            <button className="btnInner" onClick={sendData}>
+              LogIn
+            </button>
+          </Link>
+          <div className="text_container">
+            <h4 className="text">
+              Alredy SignUp?{" "}
+              <span
+                onClick={() => {
+                  console.log("Hellooo");
+                }}
+              >
+                Login here
+              </span>
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
