1bf4a5811e27f3b2fa54b18faada7f0777dc2464
shehan82 <shehansandeepa82@gmail.com>
2021-04-18 22:51:15 +0530

    change the axios url to the heroku API url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:9001"
-})
+  // baseURL: "http://localhost:9001"
+
+  baseURL: "https://myschooly.herokuapp.com/",
+});
 
 export default instance;
